4b22e0e578886024d8dc897661117b9d55e11970
Dongnyeok Shin <dn.shin98@gmail.com>
2022-11-18 23:45:38 +0900

    [set] new project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by NYEOK on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

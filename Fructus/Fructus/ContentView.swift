9e7697d9962bb7f61053ad52fb702b50022921a6
nyeok98 <dn.shin98@gmail.com>
2022-12-31 15:50:35 +0900

    [set] defaults for card sections app

---
+++ Fructus/Fructus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by 신동녘 on 2022/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 신동녘 on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

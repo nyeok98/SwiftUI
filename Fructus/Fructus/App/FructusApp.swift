bc8a23e16dfd7b4ff912420b5ce2952b52b288ca
nyeok98 <dn.shin98@gmail.com>
2023-01-01 23:59:43 +0900

    [add] App Storage

--- Fructus/Fructus/App/FructusApp.swift
+++ Fructus/Fructus/App/FructusApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct FructusApp: App {
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
     var body: some Scene {
         WindowGroup {
             OnboardingView()
